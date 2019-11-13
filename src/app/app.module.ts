2ea26095211d9780d9c002b59e18baa6ea3019fe
Lucas <lucasbonacorsi@hotmail.com>
2019-11-13 10:28:21 -0300

    task3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InicioComponent } from './inicio/inicio.component';
 import { MascotasListarComponent } from './mascotas/mascotas-listar/mascotas-listar.component';
 import { HttpClientModule } from '@angular/common/http';
-import { MascotasService } from './mascotas/mascotas.service';
+import { MascotasService } from './mascotas/shared/mascotas.service';
 
 @NgModule({
   declarations: [
