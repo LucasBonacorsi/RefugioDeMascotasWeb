b344548dd1bfe905d286bb3ed1c1244b5d58bcff
Lucas <lucasbonacorsi@hotmail.com>
2019-11-19 11:04:08 -0300

    crlIfError agregados

--- src/app/validator/custom-validator.ts
+++ src/app/validator/custom-validator.ts
@@ -1,8 +1,8 @@
 import { AbstractControl } from "@angular/forms";
 
 export class CustomValidator {
-  static ageValidator: any;
-  ageValidator(control: AbstractControl): { [key: string]: boolean } | null {
+
+ static ageValidator(control: AbstractControl): { [key: string]: boolean } | null {
     if (
       control.value !== null &&
       (isNaN(control.value) || control.value < 0 || control.value > 120)
